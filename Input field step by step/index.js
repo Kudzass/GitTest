6f38d894e215eb3635afde071cdbb43d23e4bce2
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2024-12-11 21:40:13 +0200

    Added form submit event listener with basic validation

--- Input field step by step/index.js
+++ Input field step by step/index.js
@@ -1,1 +1,15 @@
 const apiUrl = "https://testapi.io/api/Kudzass/resource/UsersList";
+
+document
+  .getElementById("inputForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("nameInput").value;
+    const responseDiv = document.getElementById("response");
+
+    if (!name) {
+      responseDiv.textContent = "Prašome įvesti vardą.";
+      return;
+    }
+  });
